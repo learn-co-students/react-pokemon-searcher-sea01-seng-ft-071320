6633c1cdddbc906348988edffc6bd7dbc31c7340
Alec DuBois <after.alec@gmail.com>
2020-09-13 21:24:02 -0700

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,23 +1,88 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import Search from './Search'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import Search from "./Search";
+import { Container } from "semantic-ui-react";
+
+const ENDPOINT = "http://localhost:3000/pokemon";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: [],
+    search: "",
+    name: "",
+    hp: "",
+    frontUrl: "",
+    backUrl: "",
+  };
+
+  componentDidMount() {
+    fetch(ENDPOINT)
+      .then((resp) => resp.json())
+      .then((pokemon) => this.setState({ pokemon: pokemon }));
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const pokemon = {
+      name: this.state.name,
+      hp: this.state.hp,
+      sprites: {
+        front: this.state.frontUrl,
+        back: this.state.backUrl,
+      },
+    };
+
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(pokemon),
+    };
+
+    fetch(ENDPOINT, configObj)
+      .then((resp) => resp.json())
+      .then((pokemon) =>
+        this.setState({
+          pokemon: [pokemon, ...this.state.pokemon],
+        })
+      )
+      .catch(console.log);
+
+    this.setState({
+      name: "",
+      hp: "",
+      frontUrl: "",
+      backUrl: "",
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm
+          {...this.state}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
         <br />
-        <Search />
+        <Search handleChange={this.handleChange} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection {...this.state} />
       </Container>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
